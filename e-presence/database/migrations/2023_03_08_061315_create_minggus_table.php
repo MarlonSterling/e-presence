70d61b6f3b7b5a58ef91929b41780effb4dc0533
Marlon <82552567+MarlonSterling@users.noreply.github.com>
2023-03-08 23:42:48 +0700

    First Commit

---
+++ e-presence/database/migrations/2023_03_08_061315_create_minggus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('minggus', function (Blueprint $table) {
+            $table->id();
+            $table->integer('NIS');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minggus');
+    }
+};
